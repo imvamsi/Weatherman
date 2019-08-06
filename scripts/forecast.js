7ec3da02d32574f1193ed93f090906a09019ca10
Vamsikrishna Tamil selvan <vamsikrishnatamilselvan@Vamsikrishnas-MacBook-Air.local>
2019-08-06 16:10:40 -0400

    get current conditions

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,14 +1,29 @@
 const key = "EFJGmNAeFXaJr9Ksp65dGZog53VG5AA5";
 
+//current info
+
+const getWeather = async id => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//city info
 const getCity = async city => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
 
   const query = `?apikey=${key}&q=${city}`;
   const response = await fetch(base + query);
   const data = await response.json();
-  return data;
+  return data[0];
 };
 
 getCity("chennai")
+  .then(data => getWeather(data.Key))
   .then(data => console.log(data))
   .catch(err => console.log("Varla da sunni"));
+
+//getCurrentConditions(206671);
